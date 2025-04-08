4d62cf48c4b7332bd80bec48ae474020f0e74659
AhmedAbdeen7 <127099805+AhmedAbdeen7@users.noreply.github.com>
2025-04-08 16:04:41 +0200

    made a modification to avoid security leaks

--- FrontEnd/src/Services/firebase.js
+++ FrontEnd/src/Services/firebase.js
@@ -35,13 +35,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpeLnkZ0f9iUCHWRPouGGhepOhNiO2bPo",
-  authDomain: "connectr-4eb4b.firebaseapp.com",
-  projectId: "connectr-4eb4b",
-  storageBucket: "connectr-4eb4b.firebasestorage.app",
-  messagingSenderId: "203190041520",
-  appId: "1:203190041520:web:00920d401184107ef8b475",
-  measurementId: "G-W8CDDLSH98"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
